f9e272955adb564217272a099f306eba4adfa450
kaviyajaya33 <kaviyakaavi33@gmail.com>
2017-06-07 09:58:04 +0530

    Add files via upload

---
+++ pgm7.java
@@ -0,0 +1,12 @@
+package trial;
+import java.util.Scanner;
+public class pgm7 {
+public static void main(String[]args){
+	int i;
+	String c="hello";
+	int b=c.length();
+	for(i=0;i<5;i++){
+		System.out.print("" +c.substring(0,b));
+	}
+}
+}
