08de611c0b04df203a5f8231cbea0d470eb997ae
kaviyajaya33 <kaviyakaavi33@gmail.com>
2017-07-05 15:12:03 +0530

    Add files via upload

---
+++ EvenNum.java
@@ -0,0 +1,17 @@
+package trial;
+import java.util.*;
+public class EvenNum {
+public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+	int a,b;
+	System.out.println("enter the start range");
+	a=in.nextInt();
+	System.out.println("enter the end range");
+	b=in.nextInt();
+	for(;a<=b;a++){
+		if(a%2==0){
+			System.out.println(a);
+		}
+	}
+}
+}
