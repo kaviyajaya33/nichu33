03513798baa973d0a21d207c568e1e3367bb451d
kaviyajaya33 <kaviyakaavi33@gmail.com>
2017-07-19 12:23:09 +0530

    Add files via upload

---
+++ FourthAndKthLargest.java
@@ -0,0 +1,19 @@
+package trials;
+import java.util.*;
+public class FourthAndKthLargest {
+public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+	int a[]=new int[10];
+	int b=a.length;
+	System.out.println("enter the height of the student");
+	for(int i=0;i<a.length;i++){
+		a[i]=in.nextInt();
+	}
+	Arrays.sort(a);
+	System.out.println(a[b-4]);
+	System.out.println("enter the position to find the num");
+	int k=in.nextInt();
+	//System.out.println(a[b-4]);
+	System.out.println(a[b-k]);
+}
+}
