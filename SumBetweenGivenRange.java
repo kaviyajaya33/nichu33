e775264951b8409e6ba895253325da17131f2b06
kaviyajaya33 <kaviyakaavi33@gmail.com>
2017-07-20 09:39:30 +0530

    Add files via upload

---
+++ SumBetweenGivenRange.java
@@ -0,0 +1,20 @@
+package guvii;
+import java.util.*;
+public class SumBetweenGivenRange {
+public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+	int sum=0,i,j=1,add=0;
+	int b[]=new int[18];
+	for( i=1;i<=15;i++){
+		sum=sum+i;
+	}
+	System.out.println(sum);
+
+for( i=15;i<=45;i++){
+if(i%2!=0){
+	System.out.println(i);
+	add=add+i;
+	
+
+}}System.out.println(add);
+}}
