4e31047675ed431d2d990afe4bb809e8f3062304
kaviyajaya33 <kaviyakaavi33@gmail.com>
2017-07-28 14:35:47 +0530

    Add files via upload

---
+++ StringToInt.java
@@ -0,0 +1,10 @@
+package guviiii;
+import java.util.*;
+public class StringToInt {
+public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+	String str=in.next();
+	int str1=Integer.parseInt(str);
+	System.out.println(str1);
+}
+}
