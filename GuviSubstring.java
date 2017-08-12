cc02077ca057e1c7175bb48dce05452dfd49bd29
kaviyajaya33 <kaviyakaavi33@gmail.com>
2017-08-12 15:51:35 +0530

    Add files via upload

---
+++ GuviSubstring.java
@@ -0,0 +1,18 @@
+package program;
+import java.util.*;
+public class GuviSubstring {
+public static void main(String[]args){
+	Scanner in=new Scanner(System.in);
+	String x=in.next();
+	String y=in.next();
+if(y.contains(x)==true){
+	System.out.println("x is a substring of y");
+}
+else{
+	System.out.println("x is not a substring of y");
+}
+		 
+	
+		
+	  
+  }}
